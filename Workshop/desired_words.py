eb8b589175f88450ad8cf33477282eed9a76e755
lavashik-mini <matiyukv41@gmail.com>
2024-03-19 19:11:37 +0300

    Random changes

--- Workshop/desired_words.py
+++
@@ -1,9 +0,0 @@
-class DesiredWords:
-    def __init__(self, words = set) -> None:
-        self.words = words
-        
-    def update(self, new_words: list[str]):
-        self.words = new_words
-        
-    def __getitem__(self, idx):
-        return self.words[idx]
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from aiogram import Bot
 from aiogram.enums import ParseMode
 
 import settings
-import Workshop
+from Workshop import P2Arser
 from Telegram import bot
 
-
+# * Add limits in config & update config
 coloredlogs.install(level='INFO', fmt="%(asctime)s | %(levelname)s | %(message)s")
 
 async def main() -> None:
@@ -17,7 +17,7 @@ async def main() -> None:
     tg_bot = Bot(config["Tg_bot_token"], parse_mode=ParseMode.HTML)  
    
     await asyncio.gather(
-        Workshop.analyze(config, tg_bot),
+        P2Arser(config, tg_bot).infinity_analyze(),
         bot.init(tg_bot)
     )        
 
